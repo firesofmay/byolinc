84ed445be909fda8726ca62d9e0aebbc20c1aedf
Mayank Jain <mayank@helpshift.com>
2014-12-25 14:55:20 +0530

    Add a scratch file.

---
+++ scratch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+/*
+$ cc -std=c99 -Wall scratch.c -o scratch
+$ ./scratch
+*/
+
+int main(int argc, char** argv){
+
+
+  for(int i = 0; i < 10; i++){
+    puts("Looping!");
+  }
+
+  return 0;
+}
